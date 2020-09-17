21a3c25fcd4e2f5e0df9e058efac0700f17c2835
ww1428386121 <1428386121@qq.com>
2020-09-17 10:07:46 +0800

    Initial commit

---
+++ VPC.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  version = "3.6.0"
+  region  = "ap-northeast-1"
+}
+
+resource "aws_vpc" "vpc_test" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames =true
+
+  tags = {
+    Name = "vpc_test"
+  }
+}
+
