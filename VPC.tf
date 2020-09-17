69a8b9fb371a8eb31a579ff16a3bd9e0310c1e01
ww1428386121 <1428386121@qq.com>
2020-09-17 11:16:19 +0800

    13

--- VPC.tf
+++ VPC.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   version = "3.6.0"
+  region  = "ap-northeast-1"
 }
 
 resource "aws_vpc" "vpc_test" {
@@ -7,7 +8,7 @@ resource "aws_vpc" "vpc_test" {
   instance_tenancy = "default"
   enable_dns_support = true
   enable_dns_hostnames =true
-  region  = "ap-northeast-1"
+
 
   tags = {
     Name = "vpc_test"
