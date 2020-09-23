c5e766c47d5adb6b1bbb52ef37ab1d73899ec490
ww1428386121 <1428386121@qq.com>
2020-09-23 12:42:13 +0800

    26

--- yum.sh
+++ yum.sh
@@ -5,4 +5,4 @@ yum -y install lrzsz httpd
 systemctl start httpd
 systemctl enable httpd
 systemctl disable firewalld
-echo hello world > /var/www/html/html.index
+echo hello world > /var/www/html/index.html
